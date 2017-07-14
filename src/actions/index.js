b75b29b180aa79a8f99e9a2f60a94702dd670961
David Sawatske <sawatskeda10@gmail.com>
2017-07-13 19:12:30 -0700

    Change API_KEY to ensure uniqueness

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
-const API_KEY = '?key=TESTING123';
+const API_KEY = '?key=FUD123';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
