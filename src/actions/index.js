f2f9a48e5b0886fe06655a7cfdc5fa762353c429
David Sawatske <sawatskeda10@gmail.com>
2017-07-13 18:23:27 -0700

    Fix error in root url to ensure API connection

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.herokuapp/api';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
-const API_KEY = '?key=fud123';
+const API_KEY = '?key=TESTING123';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`)
