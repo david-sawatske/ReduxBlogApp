b37343a3f6bbfe27aaefc4cb15352dedbdb43382
David Sawatske <sawatskeda10@gmail.com>
2017-07-16 17:22:38 -0700

    Add validations for form elements

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories "
           component={this.renderField}
         />
         <Field
@@ -38,6 +38,23 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors ={};
+
+  if (!values.title) {
+    errors.title = 'Enter a title'
+  }
+  if (!values.categorise) {
+    errors.categorise = 'Enter a categorise'
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content'
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
