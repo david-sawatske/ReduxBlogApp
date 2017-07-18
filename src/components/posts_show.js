cdae9e6781079a4cdbbe6efc36608f80510f4975
David Sawatske <sawatskeda10@gmail.com>
2017-07-18 13:31:35 -0700

    Fetch post by URL id token and render its properties

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import  { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    this.props.fetchPost();
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
   }
 
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
       <div>
-        {this.props.post.title}
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <div>{post.content}</div>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
